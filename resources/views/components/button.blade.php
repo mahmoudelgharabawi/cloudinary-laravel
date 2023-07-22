9298b56c35bcca64ab887b3ce53c0b4bd903a265
Mahmoud Ahmed Elgharabawi <37341935+mahmoudelgharabawi@users.noreply.github.com>
2023-07-22 15:16:56 +0300

    Update button.blade.php

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,7 +2,12 @@
 
     var cloudName = @json(Str::after(config('cloudinary.cloud_url'),'@'));
     var uploadPreset = @json(config('cloudinary.upload_preset'));
-
+    function setCookie(cname, cvalue, exdays) {
+      const d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+     let expires = "expires="+d.toUTCString();
+     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
     function openWidget() {
         window.cloudinary.openUploadWidget(
             { cloud_name: cloudName,
@@ -12,6 +17,7 @@
               if (!error && result && result.event === "success") {
                 console.log('Done uploading..');
                 localStorage.setItem("cloud_image_url", result.info.url);
+                setCookie("imageUrl",result.info.url,1)
               }
         }).open();
     }
